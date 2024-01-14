7306c01f3ec56c3ad6463d2526c0b7aa07584df4
Aaron Neville <aaron@codestuff.dev>
2024-01-14 16:05:31 -0700

    fix(iframe): remove non-valid props

--- src/components/FilmPoster.tsx
+++ src/components/FilmPoster.tsx
@@ -1,5 +1,5 @@
 export default function FilmPoster() {
   return (
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/kdedgBKsLGk?si=1zBnZ5RAWAaMCyHY&amp;controls=0&amp;start=1472" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/kdedgBKsLGk?si=1zBnZ5RAWAaMCyHY&amp;controls=0&amp;start=1472" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -72,9 +72,7 @@ export default function Home() {
                         height="100%"
                         src="https://www.youtube.com/embed/kdedgBKsLGk?si=7nRstBQB1_YicDFI"
                         title="YouTube video player"
-                        frameborder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        allowfullscreen
                       ></iframe>
                     </div>
                   </div>
